141cef13d1fd9a3f3380c8b26d243250d8402329
tomasroma64 <tomasroma64@gmail.com>
2022-11-27 15:24:57 +0000

    better loading screen

--- src/Loading/LoadingAnimation.tsx
+++ src/Loading/LoadingAnimation.tsx
@@ -1,9 +1,21 @@
 const LoadingAnimation = () => {
-  // TODO: Implement this component
   return (
-    <h4>
-      <i>Loading...</i>
-    </h4>
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <p>...</p>
+    </div>
   );
 };
 
